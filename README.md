0e2ecd18c77d1c3c065aefa7cbde41d696380411
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-12-31 00:07:28 +0100

    💎 "Arzigogolate" the code
    kek

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# form.ryg.one
-Flask app which allows authentication before compiling a form.
+# RYGforms
+
+Ask for a OAuth2 login, then redirect to a Typeform having one or more hidden fields.
+
+## Running
+
+### Development
+
+1. Clone this repository:
+   ```bash
+   git clone git@github.com:RYGhub/rygforms.git
+   ```
+
+2. Enter the cloned directory:
+   ```bash
+   cd rygforms
+   ```
+
+3. Create a new `.env` file inside containing the configuration (see [the example](EXAMPLE.env)):
+   ```bash
+   cp EXAMPLE.env .env
+   ```
+
+4. Install the requirements using Poetry:
+   ```bash
+   poetry install
+   ```
+
+5. Run the debug server from inside the Poetry environment:
+   ```bash
+   poetry shell
+   python -m rygforms
+   ```
+
+### Production
+
+1. Create a new venv and enter it:
+   ```bash
+   python -m venv venv
+   source venv/bin/activate
+   ```
+
+2. Install through PyPI:
+   ```bash
+   pip install rygforms gunicorn
+   ```
+   
+3. Copy the [provided systemd unit file](web-rygforms.service) to the `/etc/systemd/system` directory:
+   ```bash
+   curl https://raw.githubusercontent.com/RYGhub/rygforms/master/web-rygforms.service > /etc/systemd/system/web-rygforms.service
+   ```   
+
+4. Reload the systemd unit files:
+   ```bash
+   systemctl daemon-reload
+   ```
+
+4. Start (and optionally enable) the service:
+   ```bash
+   systemctl start "web-rygforms"
+   systemctl enable "web-rygforms"
+   ```
+
+6. Copy the [provided Apache site file](rp-rygforms.conf) to the `/etc/apache2/sites-available` directory:
+   ```bash
+   curl https://raw.githubusercontent.com/RYGhub/rygforms/master/rp-rygforms.conf > /etc/apache2/sites-available/rp-rygforms.conf
+   ```
+
+7. Enable the `rp-rygforms` site reload the Apache configuration:
+   ```bash
+   a2ensite rp-rygforms
+   systemctl reload apache2
+   ```
--- rygforms/__main__.py
+++ rygforms/__main__.py
@@ -2,25 +2,26 @@ import flask as f
 import authlib.integrations.flask_client
 import werkzeug.middleware.proxy_fix
 import os
+import dotenv
+
+dotenv.find_dotenv(raise_error_if_not_found=True)
 
 app = f.Flask(__name__)
 
-app.secret_key = os.environ['SECRET_KEY']
-clientId = os.environ['CLIENT_ID']
-clientSecret = os.environ['CLIENT_SECRET']
+app.secret_key = os.getenv('SECRET_KEY')
 
 oauth = authlib.integrations.flask_client.OAuth(app=app)
 ryg_login = oauth.register(
     name="ryg_login",
-    api_base_url="https://ryg.eu.auth0.com",
-    authorize_url='https://ryg.eu.auth0.com/authorize',
-    access_token_url="https://ryg.eu.auth0.com/oauth/token",
-    server_metadata_url="https://ryg.eu.auth0.com/.well-known/openid-configuration",
+    api_base_url=os.getenv("API_BASE_URL"),  # https://ryg.eu.auth0.com
+    authorize_url=os.getenv("AUTHORIZE_URL"),  # https://ryg.eu.auth0.com/authorize
+    access_token_url=os.getenv("ACCESS_TOKEN_URL"),  # https://ryg.eu.auth0.com/oauth/token
+    server_metadata_url=os.getenv("SERVER_METADATA_URL"),  # https://ryg.eu.auth0.com/.well-known/openid-configuration
     client_kwargs={
         "scope": "profile email openid",
     },
-    client_id=clientId,
-    client_secret=clientSecret
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET")
 )
 
 reverse_proxy_app = werkzeug.middleware.proxy_fix.ProxyFix(app=app, x_for=1, x_proto=0, x_host=1, x_port=0, x_prefix=0)
@@ -28,8 +29,8 @@ reverse_proxy_app = werkzeug.middleware.proxy_fix.ProxyFix(app=app, x_for=1, x_p
 
 @app.route("/<string:form_user>/<string:form_id>")
 def page_form(form_user: str, form_id: str):
-    f.session['form_user'] = form_user
-    f.session['form_id'] = form_id
+    f.session["form_user"] = form_user
+    f.session["form_id"] = form_id
     return ryg_login.authorize_redirect(redirect_uri=f.url_for("page_auth", _external=True), audience="")
 
 
@@ -39,10 +40,14 @@ def page_auth():
     userdata = ryg_login.get("userinfo").json()
     form_user = f.session["form_user"]
     form_id = f.session["form_id"]
-    username = userdata["nickname"]
-    user_id = userdata["sub"]
-    return f.redirect(f"https://{form_user}.typeform.com/to/{form_id}#username={username}&id={user_id}")
+    user_nickname = userdata["nickname"]
+    user_sub = userdata["sub"]
+    return f.redirect(f"https://{form_user}.typeform.com/to/{form_id}#nickname={user_nickname}&sub={user_sub}")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1')
+    # noinspection PyUnreachableCode
+    if __debug__:
+        app.run(debug=True, host="127.0.0.1", port=30012)
+    else:
+        raise Exception("This app shouldn't be run standalone in production mode.")
